bc0e77570264053ddbe5211f9d4b4a413b9acaee
SerhiiPysanets <pisanets@outlook.com>
2024-01-04 20:42:27 +0200

    feat: add component Test

--- client/config/root.jsx
+++ client/config/root.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Test from '../components/test'
 
 const Root = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>Thit is React Router v6 </div>} />
+        <Route path="/" element={Test} />
         <Route path="/test" element={<div>Thit is Test </div>} />
         <Route path="*" element={<h3>404</h3>} />
       </Routes>
