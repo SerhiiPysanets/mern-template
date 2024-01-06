36d043109ed11712294ebdfcab5e09aac40d36a0
SerhiiPysanets <pisanets@outlook.com>
2024-01-06 18:01:36 +0200

    chore del yarn.lock

--- client/components/test.jsx
+++ client/components/test.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 
-const Test = () => (
-  
-    <div className="flex justify-center items-center h-screen">
-      <div className="flex flex-col justify-center bg-indigo-800 p-10 rounded-xl select-none">
-        <span className="text-white text-right font-semibold">Boilerplate</span>
+const Test = () => {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <div className="flex flex-col justify-center bg-indigo-800 p-10 rounded-xl select-none">
+          <span className="text-white text-right font-semibold">Boilerplate</span>
+        </div>
       </div>
-    </div>
-)
+    </>
+  )
 
-Test.propTypes = {}
+}
+
+// Test.propTypes = {}
 
 export default Test
